0d03b4f093f13d2dcd6136c3a98dfa75b1d926da
YudjinOr <138213099+YudjinOr@users.noreply.github.com>
2023-08-17 12:32:23 +0000

    practised first JS code

---
+++ index.js
@@ -0,0 +1,26 @@
+const dataTypes = {
+    String: "String",
+    Number: 77,
+    Boolean: true,
+    Undefined: undefined,
+    Null: null,
+    Big_Int: 12345678901234567890n
+};
+
+const secMin = 60;
+const minOur = 60;
+const secH = secMin * minOur;
+const ourDay = 24;
+const secD = secH * ourDay;
+let dayMon = 30;
+const secM = secD * dayMon;
+
+const isAdult = confirm("Are you already 18 years old?");
+alert(isAdult);
+
+let logIn = prompt("Write your login name, please.");
+let email = prompt("Write your email adress, please.");
+let password = prompt("Insert your password, please.");
+alert(`Dear ${logIn}, your email is ${email}, your password is ${password}.`);
+//let message = `Dear ${logIn}, your email adress is ${email}, your password is ${password}.`;
+//alert(message);
